2b3ad5c9ee663d6011a65a840de52af35c0df17d
Wil Macedo <wil.macedo.sa@gmail.com>
2024-02-11 15:15:12 -0300

    chore: :zap: test with has injected web3

--- src/substrate/connect.ts
+++ src/substrate/connect.ts
@@ -1,9 +1,13 @@
+import { NotInjectedError } from '@/errors';
 import { NoAvailableAccountsError } from '@/errors/no-accounts-available-error';
 import { NoProviderAvailableError } from '@/errors/no-provider-available-error';
 import { web3Accounts, web3Enable } from '@polkadot/extension-dapp';
 import type { SubstrateAccountWithMeta } from './types';
 
 export async function connect(appName: string): Promise<SubstrateAccountWithMeta[]> {
+  if ((window as any).injectedWeb3)
+    throw new NotInjectedError()
+
   const isInjected = await web3Enable(appName)
   if (!isInjected || isInjected.length === 0)
     throw new NoProviderAvailableError()
